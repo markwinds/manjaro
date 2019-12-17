658037dcd89e611b021a035c70127a906f06776e
mark <1464089197@qq.com>
2019-12-17 22:53:29 +0800

    更新软件包

--- startManjaro2.sh
+++ startManjaro2.sh
@@ -5,7 +5,7 @@
 
 # 安装软件
 echo -e "\033[34m ****************************begin to pacman********************************* \033[0m"
-for p in fcitx fcitx-im fcitx-configtool fcitx-googlepinyin code chromium spotify netease-cloud-music telegram-desktop codeblocks xterm openbsd-netcat vim
+for p in fcitx fcitx-im fcitx-configtool fcitx-googlepinyin code chromium spotify netease-cloud-music telegram-desktop codeblocks xterm openbsd-netcat vim create_ap net-tools base-devel wireless_tools
 do 
     echo -e "\033[34m >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> $p \033[0m"
     sudo pacman -S --noconfirm $p # --noconfirm安装的时候不做交互直接安装
